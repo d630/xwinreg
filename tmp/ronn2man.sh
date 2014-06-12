704d8725dadaa7c40ed7a5c4faa4662d44d20053
D630 <D630@users.noreply.github.com>
2014-06-12 15:02:03 +0200

    0.1.9.6:xwinreg: create main manpage

--- tmp/ronn2man.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-_pwd=$(pwd)
-cp -- "$1" "${1%.*}.ronn"
-ronn --roff "${1%.*}.ronn"
-rm -- "${1%.*}.ronn"
-mv -- "${1%.*}.1" "${_pwd%/*}/doc/man/"
-man -l "${_pwd%/*}/doc/man/${1%.*}.1"
